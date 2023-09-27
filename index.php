04ed3781dc4a54d01279d7bc510bdddc1ab7ff60
masooma zahedi <masooma.zahedi1@gmail.com>
2023-09-27 13:18:49 -0500

    create new branch : test-1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><Restuarant-1></Restuarant-1></title>
 </head>
 <body>
     <?php
